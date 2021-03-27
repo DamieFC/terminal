50e11f43a2254f0b62ae3e7424e18dc3b96993a9
GNUOverYooouu <dtortoise99@gmail.com>
2021-03-27 14:49:06 -0700

    Create chickensquawk.c

---
+++ src/cmds/chickensquawk.c
@@ -0,0 +1,49 @@
+/********************************************************************************
+ * The chicken squawk command, based on the MDC song "Chicken Squawk".          *
+ * Command (c) 2021 DamieFC, Song (c) 1982 MDC                                  *
+ * When I walk into the store                                                   *
+ * They sell the chickens by the score                                          *
+ * But eating dead birds just ain't for me                                      *
+ * I don't eat roast beef or fish                                               *
+ * Porky Pig is not my dish                                                     *
+ * Just go ahead and let your chickens be                                       *
+ * In chicken circles it's Adolph Perdue                                        *
+ * Wants to feed featherless chicken to you                                     *
+ * I don't take orders from Colonel Sanders                                     *
+ * Do you?                                                                      *
+ * I don't wanna eat no hens                                                    *
+ * Not even every now or then                                                   *
+ * Wanna let all the chickens be                                                *
+ * Bawk, bawk, bawk...                                                          *
+ * Swing to the east and swing to the west                                      *
+ * Swing with the chickens you love best                                        *
+ * Come on down and do the chicken squawk with me                               *
+ * Wishing Daffy lots of luck                                                   *
+ * Cause Elmer Fudd's a hunting duck                                            *
+ * They're dreaming about their little fricassee                                *
+ * And Bugs Bunny is a friend of mine                                           *
+ * Eating him I'd feel like Frankenstein                                        *
+ * Eating flesh seems pretty foul to me                                         *
+ * So on Thanksgiving or Christmas Eve                                          *
+ * Give them turkeys a reprieve                                                 *
+ * Ain't no turkey got to die for me                                            *
+ * Foghorn, Leghorn wrote to me                                                 *
+ * Say's MDC's alright by me                                                    *
+ * Cause we all learned to let our chickens free                                *
+ * Bawk, bawk, bawk...                                                          *
+ * Swing to the east and swing to the west                                      *
+ * Swing with the chickens you love best                                        *
+ * Come on down and do the chicken squawk with me                               *
+ * Lordy Jesus this must be the end                                             *
+ * They got us eating our cartoon friends                                       *
+ * Emancipate those little chickadees                                           *
+ * Swing to the east and swing to the west                                      *
+ * Swing to the chicken you love best                                           *
+ * Come and do the chicken squawk with me                                       *
+ ********************************************************************************/
+/********************************************************************************
+ * I hope they weren't being sarcastic                                          *
+ ********************************************************************************/
+
+#include "../../include/prompt.h"
+
