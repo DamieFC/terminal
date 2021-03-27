6567b6b6c674c281b5ea4dad4e1a2dd3d6e7c3f7
GNUOverYooouu <dtortoise99@gmail.com>
2021-03-27 09:06:17 -0700

    Create hello.c

---
+++ src/cmds/hello.c
@@ -0,0 +1,14 @@
+/******************************************************************************
+ * Hello command                                                              *
+ * (c) DamieFC 2021                                                           *
+ ******************************************************************************/
+
+#include "../../include/prompt.h"
+
+int main(){
+    psPrompt();
+    if (input == "help"){
+        print("This is the DFCminal, a terminal developed by DamieFC on Github (https://github.com/DamieFC). How you found this, no idea. Anyway, I think this is multi platform, but I don't know.");
+        print("%s", prompt)
+    }
+}
