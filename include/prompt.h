e4ebf9410b2a52f383966bcf5e3b7a04d9210ace
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-01 09:09:23 -0700

    Update prompt.h

--- include/prompt.h
+++ include/prompt.h
@@ -10,8 +10,12 @@
 // Important variables
 char input;
 char prompt = "you@dfcterm>";
+// This next bit I got from GeeksForGeeks
+typedef enum { F, T } boolean;
+boolean cmdrunning;
 
 void psPrompt(void){
     printf("%s", prompt);
     gets("%s", input);
+    cmdrunning = T
 }
