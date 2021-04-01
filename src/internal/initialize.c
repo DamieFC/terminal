38477f93cf9e1c2a8252505b491175deac8a5776
GNUOverYooouu <dtortoise99@gmail.com>
2021-04-01 09:05:50 -0700

    Create initialize.c

---
+++ src/internal/initialize.c
@@ -0,0 +1,10 @@
+/******************************************************************
+ * This is for initialization of the shell.                       *
+ * (c) DamieFC 2021                                               *
+ ******************************************************************/
+#include "../../include/stlibio.h"
+#include "../../include/prompt.h"
+
+int main(){
+    psPrompt();
+}
