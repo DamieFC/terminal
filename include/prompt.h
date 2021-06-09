296618ef4d5be8714de6b9d51ba337e3bfd4b009
GNUOverYooouu <dtortoise99@gmail.com>
2021-06-09 15:53:38 -0700

    Fixed stuff

--- include/prompt.h
+++ include/prompt.h
@@ -5,14 +5,16 @@
 #ifndef PROMPT_H
 #define PROMPT_H
 
-#include "../include/io.h"
+#include "io.h"
 
 // Important variables
-char input;
-char prompt = "you@dfcterm>";
+char input[];
+char prompt[] = "you@dfcterm>";
 // This next bit I got from GeeksForGeeks
 typedef enum { F, T } boolean;
 boolean cmdrunning;
+
+void psPrompt(void);
 
 void psPrompt(void){
     printf("%s", prompt);
